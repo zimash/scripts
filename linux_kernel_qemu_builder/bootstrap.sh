0d4df03c9b0225ea04553035dd35f4a5fbb904ab
Maksim Tarelov <11013947+zimash@users.noreply.github.com>
2025-04-10 12:02:47 +0300

    bootstrap.sh: Add script
    Prepares linux/qemu source directories, builds the Docker image and runs it with the mounted source directories .

---
+++ linux_kernel_qemu_builder/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# TODO: automatically check for docker or podman on host system
+RUNNER="docker"
+
+if [ -d linux ]; then                                                         
+    cd linux && git pull --rebase ; cd ..                                     
+else                                                                          
+    git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+fi                                                                            
+
+if [ -d qemu ]; then                               
+    cd qemu && git pull --rebase ; cd ..           
+else                                               
+    git clone https://gitlab.com/qemu-project/qemu 
+fi                                                 
+
+docker build . -t linux_qemu_dev
+docker run -it --rm -v $(pwd):/sources linux_qemu_dev /bin/bash
