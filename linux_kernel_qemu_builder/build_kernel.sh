7907f562834789c04b3837945fb80801b43c7a6a
Maksim Tarelov <zimash@gmail.com>
2025-04-10 15:09:50 +0300

    build_kernel.sh: Add script to build linux kernel
    Build x86_64, riscv64, arm64 kernels.

---
+++ linux_kernel_qemu_builder/build_kernel.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cd /source/linux && mkdir -p build
+
+case $1 in
+	x86_64)
+		arch="x86_64"
+		compiler=""
+		config="defconfig"
+		;;
+	riscv64)
+		arch="riscv64"
+		compiler="riscv64-linux-gnu-"
+		config="defconfig"
+		;;
+	arm64)
+		arch="arm64"
+		compiler="aarch64-linux-gnu-"
+		config="defconfig"
+		;;
+	*)
+		echo "Can not found architecture"
+		exit
+		;;
+esac
+
+make ARCH=$arch CROSS_COMPILE=$compiler O=build $config
+make ARCH=$arch CROSS_COMPILE=$compiler O=build -j $(nproc)
+
+cd ../
