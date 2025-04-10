8514f3882f3186fcb519e7b9abefd41f3e97121d
Maksim Tarelov <11013947+zimash@users.noreply.github.com>
2025-04-10 13:51:15 +0300

    bootstrap.sh: Remove whitespaces

--- linux_kernel_qemu_builder/bootstrap.sh
+++ linux_kernel_qemu_builder/bootstrap.sh
@@ -3,17 +3,17 @@
 # TODO: automatically check for docker or podman on host system
 RUNNER="docker"
 
-if [ -d linux ]; then                                                         
-    cd linux && git pull --rebase ; cd ..                                     
-else                                                                          
+if [ -d linux ]; then
+    cd linux && git pull --rebase ; cd ..
+else
     git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
-fi                                                                            
+fi
 
-if [ -d qemu ]; then                               
-    cd qemu && git pull --rebase ; cd ..           
-else                                               
-    git clone https://gitlab.com/qemu-project/qemu 
-fi                                                 
+if [ -d qemu ]; then
+    cd qemu && git pull --rebase ; cd ..
+else
+    git clone https://gitlab.com/qemu-project/qemu
+fi
 
 docker build . -t linux_qemu_dev
 docker run -it --rm -v $(pwd):/sources linux_qemu_dev /bin/bash
